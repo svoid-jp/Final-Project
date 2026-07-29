382569dd1517257c07d8c85da89d1816494aa52a
svoid-jp <sweetapple4324@gmail.com>
2026-07-29 21:52:26 +0545

    Delete getting started with python directory

--- getting started with python/ex_2.5.py
+++
@@ -1,9 +0,0 @@
-#Calculate the average of 5 subjects where marks are: 78, 85, 62, 90, 88
-num_1=78
-num_2= 85
-num_3=62
-num_4=90
-num_5=88
-avg= (num_1+num_2+num_3+num_4+num_5)/5
-print (avg) 
-    
--- getting started with python/ex_2_4.py
+++
@@ -1,6 +0,0 @@
-#Calculate the total seconds in 3 hours, 20 minutes and 45 seconds.
-hour = 3
-minute = 20
-second = 45
-total_second = (hour*60*60)+(minute*60)+second
-print (total_second)
